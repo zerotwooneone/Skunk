efb8edd5cc883ce80a10c9a1943fd815c5d623a3
zero <squiredd@gmail.com>
2024-01-28 17:21:38 -0800

    add new formaldehyde sensor control

--- source/skunkapp/src/app/home/home.component.ts
+++ source/skunkapp/src/app/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { TestTextComponent } from '../sensors/test-text/test-text.component';
 import { BackendService } from '../backend/backend.service';
-import { Observable, filter, map } from 'rxjs';
+import { Observable, distinct, filter, map } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environment';
 import { SensorsConfig, SensorConfig } from "../../environments/SensorsConfig";
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { KnobModule } from 'primeng/knob';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'sk-home',
   standalone: true,
-  imports: [TestTextComponent, CommonModule, MatProgressBarModule],
+  imports: [TestTextComponent, CommonModule, MatProgressBarModule, KnobModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -47,8 +49,9 @@ export class HomeComponent {
     this.formaldehydeSensorValue$ = backend.SensorData$.pipe(
       filter(p => (typeof p.Formaldehyde) == 'number'),
       map(p => {
-        console.warn(`Formaldehyde:${p.Formaldehyde}`);
-        return (p.Formaldehyde as number) / (this.formaldehydeSensorConfig.MaxValue ?? 1);
-      }));
+        //console.warn(`Formaldehyde:${(p.Formaldehyde as number)} max:${this.formaldehydeSensorConfig.MaxValue}`);
+        return (p.Formaldehyde as number);
+      }),
+      distinct());
   }
 }
